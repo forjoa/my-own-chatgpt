c27732708d4035f82793c8e79a43561f865dff88
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-03 13:32:46 +0100

    Initialized project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// styles
+import './App.css'
+
+// imports
+// import Configuration, { OpenAIApi } from "openai"
+
+
+function App() {
+
+  return (
+    <>
+      <p>hello</p>
+    </>
+  )
+}
+
+export default App
